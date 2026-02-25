ebe4859c24c1778bffbb841a8684334bd7dd522b
Iqbal Hossen <iqbalhossen60483@gmail.com>
2026-02-25 23:07:56 +0600

    vercel removed

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,9 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import cookieParser from 'cookie-parser';
 import { setDefaultResultOrder } from 'dns';
-import express from 'express';
 import helmet from 'helmet';
-import serverless from 'serverless-http';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './middleware/exception.filter';
-
-const expressApp = express();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -53,5 +49,3 @@ async function bootstrap() {
   });
 }
 bootstrap().catch((err) => console.log(err));
-
-export const handler = serverless(expressApp);
